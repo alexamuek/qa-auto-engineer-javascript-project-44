de871d1d1f9b86988f702237c2894fc1e099253e
Alex <alexamuek@gmail.com>
2024-02-04 23:18:07 +0800

    update brain-event

---
+++ src/doBrainEventGame.js
@@ -0,0 +1,43 @@
+import readlineSync from 'readline-sync';
+import getRandomInt from './getRandomInt.js';
+
+const doBrainEventGame = () => {
+  const minValue = 0;
+  const maxValue = 100;
+
+  let randomNumber;
+  let userAnswer;
+  let expectedAnswer;
+  let i = 0;
+
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  while (i < 3) {
+    randomNumber = getRandomInt(minValue, maxValue);
+
+    if (randomNumber % 2 === 0) {
+      expectedAnswer = 'yes';
+    } else {
+      expectedAnswer = 'no';
+    }
+
+    readlineSync.question(`Question: ${randomNumber}`);
+    userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer.toLowerCase() === expectedAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'`);
+      break;
+    }
+    i += 1;
+  }
+
+  if (i === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+export default doBrainEventGame;
