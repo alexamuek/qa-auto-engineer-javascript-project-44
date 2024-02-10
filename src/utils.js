5878a5c60ea7bd60f5189481d8ec27a416ef31cc
Alex <alexamuek@gmail.com>
2024-02-10 21:09:34 +0800

    fix by linter

--- src/utils.js
+++ src/utils.js
@@ -24,18 +24,22 @@ const sendNegativeDecision = (name, expectedAnswer, userAnswer) => {
 const sendPositiveDecision = (name) => console.log(`Congratulations, ${name}!`);
 
 const showDescription = (gameName) => {
-  switch (gameName){
+  switch (gameName) {
     case 'brain-even':
       console.log('Answer "yes" if the number is even, otherwise answer "no".');
       break;
     case 'brain-calc':
       console.log('What is the result of the expression?');
+      break;
     case 'brain-gcd':
       console.log('Find the greatest common divisor of given numbers.');
+      break;
     case 'brain-progression':
       console.log('What number is missing in the progression?');
+      break;
     case 'brain-prime':
       console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+      break;
     default:
       break;
   }
