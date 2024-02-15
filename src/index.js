4227d085d252e81b6fb4873be6f77d721837def5
Alex <alexamuek@gmail.com>
2024-02-15 18:18:59 +0800

    using for cycle

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
 import readlineSync from 'readline-sync';
 
 const gameEngine = (generateRound, description) => {
-  const userAnswer = [];
-  const roundData = [];
-  let i = 0;
-
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
 
-  while (i < 3) {
-    roundData[i] = generateRound();
-    console.log(`Question: ${roundData[i][0]}`);
-    userAnswer[i] = readlineSync.question('Your answer: ');
+  for (let i = 0; i <= 2; i += 1) {
+    const roundData = generateRound();
+    console.log(`Question: ${roundData[0]}`);
+    const userAnswer = readlineSync.question('Your answer: ');
 
-    if (userAnswer[i].toLowerCase() === roundData[i][1]) {
+    if (userAnswer.toLowerCase() === roundData[1]) {
       console.log('Correct!');
     } else {
-      console.log(`'${userAnswer[i]}' is wrong answer ;(. Correct answer was '${roundData[i][1]}'`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${roundData[1]}'`);
       console.log(`Let's try again, ${userName}!`);
-      break;
+      return;
     }
-    i += 1;
   }
-
-  if (i === 3) {
-    console.log(`Congratulations, ${userName}!`);
-  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default gameEngine;
