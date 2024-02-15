e5a25097510a618e49341245705195ca34750dc2
Alex <alexamuek@gmail.com>
2024-02-15 18:50:02 +0800

    add calculate in calcBrainGcdGame

--- src/games/callBrainGcdGame.js
+++ src/games/callBrainGcdGame.js
@@ -1,26 +1,32 @@
 import getRandomInt from '../utils.js';
 import gameEngine from '../index.js';
 
-const generateRound = () => {
-  const minValue = 0;
-  const maxValue = 100;
-  let number1;
-  let number2;
+const calculate = (number1, number2) => {
+  let operand1;
+  let operand2;
   let gcd = 0;
-
-  number1 = getRandomInt(minValue, maxValue);
-  number2 = getRandomInt(minValue, maxValue);
-  const questionPart = `${number1} ${number2}`;
-  while (Math.abs(number1 - number2) >= 0) {
-    gcd = Math.abs(number1 - number2);
+  operand1 = number1;
+  operand2 = number2;
+  while (Math.abs(operand1 - operand2) >= 0) {
+    gcd = Math.abs(operand1 - operand2);
     if (gcd === 0) {
       break;
     } else {
-      number2 = Math.min(number1, number2);
-      number1 = gcd;
+      operand2 = Math.min(operand1, operand2);
+      operand1 = gcd;
     }
   }
-  return [questionPart, String(number1)];
+  return operand1;
+};
+
+const generateRound = () => {
+  const minValue = 1;
+  const maxValue = 100;
+  const number1 = getRandomInt(minValue, maxValue);
+  const number2 = getRandomInt(minValue, maxValue);
+  const questionPart = `${number1} ${number2}`;
+  const answer = calculate(number1, number2);
+  return [questionPart, String(answer)];
 };
 
 const callBrainGcd = () => {
